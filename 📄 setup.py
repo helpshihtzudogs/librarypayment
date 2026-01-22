52e6b4e69fc8444b7a598e30030996e271bb522c
sabicedeveloper <liftface067@gmail.com>
2026-01-22 07:22:12 +0100

    Add setup.py for PayPal integration library
    Initial setup for PayPal integration library with metadata.
    
    Signed-off-by: sabicedeveloper <liftface067@gmail.com>

---
+++ 📄 setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paypal-integration",
+    version="0.1.0",
+    description="Jednostavna Python biblioteka za PayPal integraciju",
+    author="Vaše Ime",
+    url="https://github.com/VAŠ_USERNAME/paypal-integration",
+    packages=find_packages(),
+    install_requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
